8975b16e7b508e3bb62e8bd464ee4f01bff29174
Zia Zameer <Zia.Zameer@metrobank.plc.uk>
2023-04-13 13:34:38 +0100

    Add project files.

---
+++ Models/DropDownModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp2.Models
+{
+    public class DropDownModel
+    {
+        public string Id { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public bool Divider { get; set; } = false;
+
+        public bool Disabled { get; set; } = false;
+
+        public EventCallback<DropDownModel> EventCallback { get; set; }
+    }
+}
---
+++ Models/SelectModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp2.Models
+{
+    public class SelectModel
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public bool Disabled { get; set; } = false;
+        public bool Divider { get; set; } = false;
+    }
+}
