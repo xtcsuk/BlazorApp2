dd8c1401dc56e7c36ae14bd158d81c99a58f305c
Zia Zameer <Zia.Zameer@metrobank.plc.uk>
2023-06-08 17:28:23 +0100

    changes

---
+++ Pages/DropDownComponent.razor.cs
@@ -0,0 +1,36 @@
+﻿using AntDesign;
+using BlazorApp2.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp2.Pages
+{
+    public partial class DropDownComponent
+    {
+        [Parameter]
+        public string? AuthorizedRoles { get; set; }
+
+        [Parameter]
+        public IEnumerable<DropDownModel>? Data { get; set; }
+
+        [Parameter]
+        public string ButtonCaption { get; set; } = "No Caption";
+
+        [Parameter]
+        public EventCallback<DropDownModel> OnItemClick { get; set; }
+
+        private void OnItemClicked(MenuItem value)
+        {
+            if (OnItemClick.HasDelegate)
+            {
+                var model = new DropDownModel
+                {
+                    Id = value.Id,
+                    Disabled = value.Disabled
+                };
+                var description = Data?.Single(m => m.Id.Equals(model.Id)).Description;
+                model.Description = description ?? string.Empty;
+                OnItemClick.InvokeAsync(model);
+            }
+        }
+    }
+}
---
+++ Pages/SelectComponent.razor.cs
@@ -0,0 +1,53 @@
+﻿using BlazorApp2.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp2.Pages
+{
+    public partial class SelectComponent
+    {
+        private string _currentValue = string.Empty;
+
+        [Parameter]
+        public IEnumerable<SelectModel> Data { get; set; } = new List<SelectModel>();
+
+        [Parameter]
+        public string LabelCaption { get; set; } = string.Empty;
+
+        [Parameter]
+        public string CurrentValue { get; set; } = string.Empty;
+
+        [Parameter]
+        public bool Disabled { get; set; } = false;
+
+        [Parameter]
+        public string OptionCssClass { get; set; } = string.Empty;
+
+        [Parameter]
+        public EventCallback<SelectModel> OnItemClick { get; set; }
+
+        private void ItemClicked(string value)
+        {
+            if (value != null && !CurrentValue.Equals(value))
+            {
+                var selectedModel = Data.Single(m => m.Id.Equals(value));
+                var returnModel = new SelectModel
+                {
+                    Id = selectedModel.Id,
+                    Disabled = selectedModel.Disabled,
+                    Description = selectedModel.Description,
+                    Divider = selectedModel.Divider
+                };
+                if (OnItemClick.HasDelegate)
+                {
+                    OnItemClick.InvokeAsync(returnModel);
+                }
+            }
+        }
+
+        protected override void OnParametersSet()
+        {
+            _currentValue = CurrentValue;
+            base.OnParametersSet();
+        }
+    }
+}
